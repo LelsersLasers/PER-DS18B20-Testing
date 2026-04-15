e1995fdfb596560ebbd8c2976a2582b340509397
LelsersLasers <millankumar@gmail.com>
2026-04-14 22:16:57 -0400

    setup prints debug info

--- PER-DS18B20-Testing.ino
+++ PER-DS18B20-Testing.ino
@@ -5,11 +5,19 @@
 #include <DallasTemperature.h>
 
 
+#define ONE_WIRE_PIN (2)
+#define NUM_SENSORS (1)
 
-void setup() {
-  // put your setup code here, to run once:
 
 }
+void setup(void) {
+    Serial.begin(9600);
+    Serial.println("Simple Teensy based setup to test our E-meter therm harness.");
+    Serial.println("Read a chain of parasitic DS18B20s.");
+    Serial.print("OneWire bus on pin: ");
+    Serial.println(ONE_WIRE_PIN);
+    Serial.print("Number of sensors: ");
+    Serial.println(NUM_SENSORS);
 
 void loop() {
   // put your main code here, to run repeatedly:
